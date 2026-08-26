76029ac105e79578e50f3d67ef839a36baea7607
Luís Dourado <luisdourado@Luiss-MacBook-Pro.local>
2026-08-26 08:18:58 -0400

    fix: extrai constante para o prefixo api/v1 nas rotas

--- config/urls.py
+++ config/urls.py
@@ -7,14 +7,16 @@ from django.urls import path
 from drf_spectacular.views import SpectacularAPIView
 from drf_spectacular.views import SpectacularSwaggerView
 
+_API_V1 = "api/v1/"
+
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
-    path("api/v1/", include("apps.core.api.urls", namespace="core")),
-    path("api/v1/", include("apps.sigpae.api.urls", namespace="sigpae")),
-    path("api/v1/", include("config.api_router")),
-    path("api/v1/schema/", SpectacularAPIView.as_view(), name="api-schema"),
+    path(_API_V1, include("apps.core.api.urls", namespace="core")),
+    path(_API_V1, include("apps.sigpae.api.urls", namespace="sigpae")),
+    path(_API_V1, include("config.api_router")),
+    path(f"{_API_V1}schema/", SpectacularAPIView.as_view(), name="api-schema"),
     path(
-        "api/v1/docs/",
+        f"{_API_V1}docs/",
         SpectacularSwaggerView.as_view(url_name="api-schema"),
         name="api-docs",
     ),
